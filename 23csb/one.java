1955d1714b395e43fb3065be17cb455977f6e35d
stevekalix <msr962005@gmail.com>
2026-03-10 11:25:18 +0530

    one file updates

--- 23csb/one.java
+++ 23csb/one.java
@@ -1,17 +1,11 @@
 class one{
     public static void main(String[] args) {
-        int a=10;
-        int b=20;
-        int c=a+b;
-        System.out.println("the sum of a and b is "+c);
-        one obj=new one();
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("the sum of a and b is " + c);
+        one obj = new one();
         obj.Sum(a, b);
 
-    }
-
-
-    void Sum(int a,int b){
-        int c=a+b;
-        System.out.println("the sum of a and b is "+c);
     }
 }
--- 23csb/two.java
+++
@@ -1,9 +0,0 @@
-class two {
-    public static void main(String a[]) {
-        Sys.out.println("Hello world ");
-        two obj = new two();
-        int x = 10;
-
-
-    }
-}
