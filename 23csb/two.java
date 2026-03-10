edbfc9bf9bdb18dc9854a57306665c98c0ba706a
stevekalix <msr962005@gmail.com>
2026-03-10 11:23:54 +0530

    deleted message commited herer

--- 23csb/two.java
+++ 23csb/two.java
@@ -1,33 +1,9 @@
-class two{
-public static void main(String a[]){
-  Sys.out.println("Hello world ");
-  two obj=new two();
-    int x=10;
-    int y=20;
-    obj.Sum(x, y);
-    obj.Sub(x, y);
-    obj.Mul(x, y);
+class two {
+    public static void main(String a[]) {
+        Sys.out.println("Hello world ");
+        two obj = new two();
+        int x = 10;
 
-}
 
-void Sum(int a,int b) {
-  int c = a + b;
-  System.out.println("the sum of a and b is " + c);
-}
-
-void Sub(int a,int b) {
-  int c = a - b;
-  System.out.println("the difference of a and b is " + c);
-}
- void Mul(int a,int b) {
-  int c = a * b;
- }
-
-  //asdada9gdbadan
-  asssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-  kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkKKKKKKKKKKKKKKKKKKKKKKKKKKKkkkkkkkkkkkkkkkkkkkKKKKKKKKKKKKKKKKKKKKKKKKK
-          a
-    sadasd
-                  adma,dla,sl,as
-    dmasmdkasmld,adamdkas
+    }
 }
