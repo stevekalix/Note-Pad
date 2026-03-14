c9f7ab8dd26085d7ac0e2914933acd0dd5d76e36
stevekalix <msr962005@gmail.com>
2026-03-14 10:05:00 +0530

    add a+b in this 23csb folder

--- 23csb/one.java
+++ 23csb/one.java
@@ -1,14 +1,10 @@
+
 class one{
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        int c = a + b;
-        System.out.println("the sum of a and b is " + c);
-        one obj = new one();
-        obj.Sum(a, b);
+        System.out.prinltn(a+b);
 
-    }
-
-    String name="Manikandan";
-    int age=20;
+        }
 }
+
